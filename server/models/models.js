f5f976b0a8eebf0f7056a5fc056020f36a422235
Shamee <nachozzzz224@gmail.com>
2023-02-25 12:01:06 +0300

    patch - 2 Backend регистрация, обработака запросов, удаление и добавление товаров

--- server/models/models.js
+++ server/models/models.js
@@ -63,7 +63,7 @@ BasketDevice.belongsTo(Basket)
 Device.hasOne(BasketDevice)
 BasketDevice.belongsTo(Device)
 
-Device.hasMany(DeviceInfo)
+Device.hasMany(DeviceInfo, {as: 'info'})
 DeviceInfo.belongsTo(Device)
 
 Device.hasMany(Rating)
